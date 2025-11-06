66d881c150a56b2997dfc233555610f503da39e0
John <john.britu@gmail.com>
2025-11-06 17:35:45 -0300

    Arquivos no repo

---
+++ 3_CORE_MODULES/9_renomeando_arquivo/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const arquivoAntigo = "arquivo.txt";
+const arquivoNovo = "novoarquivo.txt";
+
+fs.rename(arquivoAntigo, arquivoNovo, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Arquivo renomeado com sucesso!");
+});
